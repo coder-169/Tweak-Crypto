f6a16aab1dcb28779c327af26c5319f891d0317b
Sajid Ali <113129396+coder-169@users.noreply.github.com>
2024-06-02 13:32:45 +0500

    Re Push it

--- app/api/wallet/withdraw/route.ts
+++ app/api/wallet/withdraw/route.ts
@@ -1,45 +1,9 @@
 import { db } from "@/lib/db";
+import { sendMoney } from "@/lib/utils";
 import { NextResponse } from "next/server";
-import crypto from "crypto";
-import { getServerTime } from "@/lib/utils";
 
-export const sendMoney = async (params:any) => {
-  try {
-    const serverTime = await getServerTime();
-    var recvWindow = 2000; // Maximum recvWindow value
-    params.timestamp = serverTime - recvWindow;
-    let query = Object.keys(params)
-      .map((key) => `${key}=${encodeURIComponent(params[key])}`)
-      .join("&");
-    console.log(process.env.WALLET_API_KEY, process.env.WALLET_SECRET_KEY);
-    const signature = crypto
-      .createHmac("sha256", process.env.WALLET_SECRET_KEY || "")
-      .update(query)
-      .digest("hex");
-    console.log(signature);
-    query += `&signature=${signature}`;
-    const url =
-      "https://api.binance.com/sapi/v1/capital/withdraw/apply?" + query;
-    console.log(url);
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "X-MBX-APIKEY": process.env.WALLET_API_KEY || "",
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-    });
-    const data = await response.json();
-    console.log(data);
-    if (response.status === 200) {
-      return { data, error: false };
-    } else {
-      return { message: data.msg, error: true };
-    }
-  } catch (error: any) {
-    console.log(error);
-    return { error: true, message: error.message };
-  }
-};
+
+
 export async function POST(req: Request) {
   try {
     const body = await req.json();
@@ -53,7 +17,7 @@ export async function POST(req: Request) {
     if (res.error) {
       return NextResponse.json({ success: false, message: res.message });
     } else {
-     await db.user.update({
+      await db.user.update({
         where: {
           externalUserId: body?.id,
         },
--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,7 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import stc from "string-to-color";
 import { NextResponse } from "next/server";
-
+import crypto from "crypto";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -10,7 +10,7 @@ export function cn(...inputs: ClassValue[]) {
 export const stringToColor = (str: string) => {
   return stc(str);
 };
-export const getServerTime = async () => {
+export const sendMoney = async (params: any) => {
   try {
     const resp = await fetch("https://testnet.binance.vision/api/v3/time", {
       method: "GET",
@@ -20,11 +20,39 @@ export const getServerTime = async () => {
       },
     });
     const dt = await resp.json();
-    return dt.serverTime;
+    const serverTime = dt.serverTime;
+
+    var recvWindow = 2000; // Maximum recvWindow value
+    params.timestamp = serverTime - recvWindow;
+    let query = Object.keys(params)
+      .map((key) => `${key}=${encodeURIComponent(params[key])}`)
+      .join("&");
+    console.log(process.env.WALLET_API_KEY, process.env.WALLET_SECRET_KEY);
+    const signature = crypto
+      .createHmac("sha256", process.env.WALLET_SECRET_KEY || "")
+      .update(query)
+      .digest("hex");
+    console.log(signature);
+    query += `&signature=${signature}`;
+    const url =
+      "https://api.binance.com/sapi/v1/capital/withdraw/apply?" + query;
+    console.log(url);
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "X-MBX-APIKEY": process.env.WALLET_API_KEY || "",
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    if (response.status === 200) {
+      return { data, error: false };
+    } else {
+      return { message: data.msg, error: true };
+    }
   } catch (error: any) {
-    return NextResponse.json(
-      { success: false, message: error.message },
-      { status: 401 }
-    );
+    console.log(error);
+    return { error: true, message: error.message };
   }
 };
